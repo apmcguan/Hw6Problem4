fc71618b5c83c92dcea5f9db13771711d1ef56b2
apmcguan <111770168+apmcguan@users.noreply.github.com>
2022-09-30 21:17:52 +0000

    finding a

--- main.cpp
+++ main.cpp
@@ -11,16 +11,16 @@
 
 using namespace std;
 
-int days(int day);
+int days(int d);
 
-int M(string M, int month);
+int M(string M, int m);
 
 int main() {
   int n;//number of classmates
-  int day; //day you were born on
-  int month_i;//moth to number
-  int M,days;
-  //string M; // month you were born**/
+  int d; //day you were born on
+  int m;//moth to number
+  string M;
+
   
   /**int month;
 int day; 
@@ -28,94 +28,185 @@ int n;
 string input;**/
 
   
-  cout << "Enter number of classmates:"<<endl;
+  //cout << "Enter number of classmates:"<<endl;
   cin>>n;
 
   
   
-  for(int i=0; i<n; i++)
+  for(int i = 0; i < n; i++)
     {
-      day = days;
-      month_i = M ;
-        
+    d = days(d);
+      m = M (M, m);
+    
+
       
-      
-     
-      
-      
-    } 
-  }
+      } 
   
+  }
+
 
   //day input
-  int days(int day)
+  int days(int d)
   {
-    cin>>day;
+    cin>>d;
    
-    return day;
+    return d;
     
     }
   //month input
-  int M(string M, int month_i)
+  int M (string M, int m)
   {
     cin>> M;
     
     if (M == "Jan")
     {
-      month_i = 1;
+      m = 1;
       }
     if (M == "Feb")
     {
-      month_i = 2;
+      m = 2;
       }
     if (M == "Mar")
     {
-      month_i = 3;
+      m = 3;
       }
     if (M == "Apr")
     {
-      month_i = 4;
+      m = 4;
       }
     if (M == "May")
     {
-      month_i = 5;
+      m = 5;
       }
     if (M == "Jun")
     {
-      month_i = 6;
+      m = 6;
       }
     if (M== "Jul")
     {
-      month_i = 7;
+      m = 7;
       }
     if (M == "Aug")
     {
-      month_i = 8;
+      m = 8;
       }
     if (M == "Sep")
     {
-      month_i = 9;
+      m = 9;
       }
     if (M == "Oct")
     {
-      month_i = 10;
+      m = 10;
       }
     if (M == "Nov")
     {
-      month_i = 11;
+      m = 11;
       }
     if (M == "Dec")
     {
-      month_i = 12;
+      m = 12;
       }
-cout<<"month"<<month_i<<endl;
     
     }
 
   
-  /**int A(int month_i, int day)
+  int A(int m, int d)
   {
-    switch(month)
+    switch(m)
+      {
+        case 1:
+  if ((d < 21) && (m == 1))
+  cout << "Capricorn" << endl;
+  else
+  cout << "Aquarius" << endl;
+  break;
+
+  case 2:
+  if ((d < 20) && (m== 2))
+  cout << "Aquarius" << endl;
+  else
+  cout << "Pisces" << endl; 
+  break;
+
+  case 3:
+  if ((d < 21) && (m == 3))
+  cout << "Pisces" << endl;
+  else
+  cout << "Aries" << endl;
+  break;
+
+  case 4:
+  if ((d < 21) && (m == 4))
+  cout << "Aries" << endl;
+  else
+  cout << "Taurus" << endl;
+  break;
+
+  case 5:
+  if ((d < 21) && (m == 5))
+  cout << "Taurus" << endl;
+  else
+  cout << "Gemini" << endl;
+  break;
+
+  case 6:
+  if ((d < 22) && (m == 6))
+  cout << "Gemini" << endl;
+  else
+  cout << "Cancer" << endl;
+  break;
+
+  case 7:
+  if ((d < 23) && (m == 7))
+  cout << "Cancer" << endl;
+  else
+  cout << "Leo" << endl;
+  break;
+  
+  case 8:
+  if ((d < 23) && (m == 8))
+  cout << "Leo" << endl;
+  else
+  cout << "Virgo" << endl;
+  break;
+
+  case 9:
+  if ((d < 22) && (m == 9))
+  cout << "Virgo" << endl;
+  else
+  cout << "Libra" << endl;
+  break;
+
+  case 10:
+  if ((d < 23) && (m == 10))
+  cout << "Libra" << endl;
+  else
+  cout << "Scorpio" << endl;
+  break;
+
+  case 11:
+  if ((d < 23) && (m  == 11))
+  cout << "Scorpio" << endl;
+  else
+  cout << "Sagittarius" << endl;
+  break;
+
+  case 12:
+  if ((d < 22) && (m == 12))
+  cout << "Sagittarius" << endl;
+  else
+  cout << "Capricorn" << endl;
+  break;
+
+  default:
+  cout << "An error has occured " << m << endl;
+  break;
+  
+}
+return m;
+        
+      }
+    
   }
-**/
+
   
