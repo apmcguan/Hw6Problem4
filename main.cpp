8edfcd70d8b8e3687c95c839bf191224e1019a7c
apmcguan <111770168+apmcguan@users.noreply.github.com>
2022-09-30 19:30:55 +0000

    month string to value

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <cmath>
 
 using namespace std;
-
+int M(string input, int month)
 int main() {
   int n, d, month_i;
   string month;
@@ -20,4 +20,59 @@ int main() {
       
       
     }  
+  int M(string input, int month)
+  {
+    cin>> month;
+    if (input == Jan)
+    {
+      month_i = 1;
+      }
+    if (input == Feb)
+    {
+      month_i = 2;
+      }
+    if (input == Mar)
+    {
+      month_i = 3;
+      }
+    if (input == Apr)
+    {
+      month_i = 4;
+      }
+    if (input == May)
+    {
+      month_i = 5;
+      }
+    if (input == Jun)
+    {
+      month_i = 6;
+      }
+    if (input == Jul)
+    {
+      month_i = 7;
+      }
+    if (input == Aug)
+    {
+      month_i = 8;
+      }
+    if (input == Sep)
+    {
+      month_i = 9;
+      }
+    if (input == Oct)
+    {
+      month_i = 10;
+      }
+    if (input == Nov)
+    {
+      month_i = 11;
+      }
+    if (input == Dec)
+    {
+      month_i = 12;
+      }
+    
+    
+  }
+  
 }
