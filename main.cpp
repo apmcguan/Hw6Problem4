c8e76452cc04e0370b67bb98222df5ff7fd4dc5f
apmcguan <111770168+apmcguan@users.noreply.github.com>
2022-09-30 19:13:03 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+  int n, d, month_i;
+  string month;
+  
+  cout << "Enter number of classmates:"<<endl;
+  cin>>n;
+
+  for(int i; i<n; i++)
+    {
+      cout<<"Enter the day you were born: ";
+      cin>>d;
+      cout<<"Enter the Month you were born: ";
+      cin>> month; 
+      
+      
+    }  
+}
